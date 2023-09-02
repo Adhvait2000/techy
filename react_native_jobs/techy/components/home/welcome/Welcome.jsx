76d770e2edb8b21fb8c17daa81907e7f65a07233
Adhvait2000 <64685259+Adhvait2000@users.noreply.github.com>
2023-09-02 12:27:38 +0800

    UI for Welcome Page

---
+++ react_native_jobs/techy/components/home/welcome/Welcome.jsx
@@ -0,0 +1,69 @@
+import  {useState} from 'react'
+import { 
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList
+ } from 'react-native'
+ import { useRouter } from 'expo-router'
+
+import styles from './welcome.style'
+import { icons, SIZES} from '../../../constants';
+
+const jobTypes = ["Full-Time", "Part-Time", "Contractor"]
+
+const Welcome = () => {
+  const router = useRouter();
+
+  const [activeJobTypes, setActiveJobTypes] = useState("Full-Time")
+  
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Adhvait</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job!</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={styles.searchInput}
+            value=""
+            onChange={()=>{}}
+            placeholder = "What are you looking for?"
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image 
+            source={icons.search}
+            resizeMode='contain'
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.container}>
+        <FlatList 
+          data={jobTypes}
+          renderItem={({item}) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobTypes, item)} 
+              onPress={() => {
+                setActiveJobTypes(item);
+                router.push(`/search/${item}`)
+              }}
+            >
+              <Text style={styles.tabText(activeJobTypes, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item}
+          contentContainerStyle = {{columnGap: SIZES.small}}
+          horizontal
+        />
+      </View>
+    </View>
+  )
+}
+
+export default Welcome
