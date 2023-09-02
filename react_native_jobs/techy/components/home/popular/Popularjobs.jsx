7be6c1496cae26e7cdccaf7c145518fe9ef36ea0
Adhvait2000 <64685259+Adhvait2000@users.noreply.github.com>
2023-09-02 13:26:27 +0800

    NearbyJobs

--- react_native_jobs/techy/components/home/popular/Popularjobs.jsx
+++ react_native_jobs/techy/components/home/popular/Popularjobs.jsx
@@ -17,6 +17,12 @@ const Popularjobs = () => {
     num_pages: 1
   })
 
+  const [selectedJob, setSelectedJob] = useState();
+
+  const handleCardPress = (item) => {
+
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
