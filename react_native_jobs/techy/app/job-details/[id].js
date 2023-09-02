6c3a9b3a4aa867f8f1a12204eb17d7bfac590ccd
Adhvait2000 <64685259+Adhvait2000@users.noreply.github.com>
2023-09-02 16:02:31 +0800

    Tabs

--- react_native_jobs/techy/app/job-details/[id].js
+++ react_native_jobs/techy/app/job-details/[id].js
@@ -6,6 +6,9 @@ import {Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from
 import { COLORS, icons, SIZES } from '../../constants';
 import useFetch from "../../hook/useFetch"; 
 
+
+const tabs = ["About", "Qualifications", "Responsibilities"]
+
 const JobDetails = () => {
 
     const params = useSearchParams();
@@ -16,6 +19,7 @@ const JobDetails = () => {
     })
 
     const [refreshing, setRefreshing] = useState(false);
+    const [activeTab, setActiveTab] = useState(tabs[0]);
 
     const onRefresh = () => {}
 
@@ -58,10 +62,12 @@ const JobDetails = () => {
                             jobTitle = {data[0].job_title}
                             companyName = {data[0].employer_name}
                             location = {data[0].job_country}
-                        
                         />
 
                         <JobTabs 
+                            tabs={tabs}
+                            activeTab = {activeTab}
+                            setActiveTab = {setActiveTab}
                         
                         />
                     </View>
